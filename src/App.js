7e5629e1c9cf087231af0981f82cfed0fea51340
Mücahit Yıldırım <mucahityildirim@Mucahit-MacBook-Air.local>
2023-02-17 15:36:32 +0300

    create project and add navbar

--- src/App.js
+++ src/App.js
@@ -1,23 +1,101 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import Navbar from "./components/Navbar/Navbar";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Navbar />
+      <h1>
+        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Iusto, ut
+        quis. Totam temporibus alias corporis vitae, nisi quibusdam libero et
+        architecto neque dignissimos similique sequi illo nostrum.
+        Exercitationem magnam unde molestiae asperiores necessitatibus excepturi
+        suscipit possimus aliquam. Voluptas corporis eligendi pariatur deleniti
+        nostrum officia, voluptatem similique officiis perspiciatis distinctio
+        cupiditate
+      </h1>
+      <h1>
+        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Iusto, ut
+        quis. Totam temporibus alias corporis vitae, nisi quibusdam libero et
+        architecto neque dignissimos similique sequi illo nostrum.
+        Exercitationem magnam unde molestiae asperiores necessitatibus excepturi
+        suscipit possimus aliquam. Voluptas corporis eligendi pariatur deleniti
+        nostrum officia, voluptatem similique officiis perspiciatis distinctio
+        cupiditate
+      </h1>
+      <h1>
+        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Iusto, ut
+        quis. Totam temporibus alias corporis vitae, nisi quibusdam libero et
+        architecto neque dignissimos similique sequi illo nostrum.
+        Exercitationem magnam unde molestiae asperiores necessitatibus excepturi
+        suscipit possimus aliquam. Voluptas corporis eligendi pariatur deleniti
+        nostrum officia, voluptatem similique officiis perspiciatis distinctio
+        cupiditate
+      </h1>
+      <h1>
+        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Iusto, ut
+        quis. Totam temporibus alias corporis vitae, nisi quibusdam libero et
+        architecto neque dignissimos similique sequi illo nostrum.
+        Exercitationem magnam unde molestiae asperiores necessitatibus excepturi
+        suscipit possimus aliquam. Voluptas corporis eligendi pariatur deleniti
+        nostrum officia, voluptatem similique officiis perspiciatis distinctio
+        cupiditate
+      </h1>
+      <h1>
+        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Iusto, ut
+        quis. Totam temporibus alias corporis vitae, nisi quibusdam libero et
+        architecto neque dignissimos similique sequi illo nostrum.
+        Exercitationem magnam unde molestiae asperiores necessitatibus excepturi
+        suscipit possimus aliquam. Voluptas corporis eligendi pariatur deleniti
+        nostrum officia, voluptatem similique officiis perspiciatis distinctio
+        cupiditate
+      </h1>
+      <h1>
+        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Iusto, ut
+        quis. Totam temporibus alias corporis vitae, nisi quibusdam libero et
+        architecto neque dignissimos similique sequi illo nostrum.
+        Exercitationem magnam unde molestiae asperiores necessitatibus excepturi
+        suscipit possimus aliquam. Voluptas corporis eligendi pariatur deleniti
+        nostrum officia, voluptatem similique officiis perspiciatis distinctio
+        cupiditate
+      </h1>
+      <h1>
+        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Iusto, ut
+        quis. Totam temporibus alias corporis vitae, nisi quibusdam libero et
+        architecto neque dignissimos similique sequi illo nostrum.
+        Exercitationem magnam unde molestiae asperiores necessitatibus excepturi
+        suscipit possimus aliquam. Voluptas corporis eligendi pariatur deleniti
+        nostrum officia, voluptatem similique officiis perspiciatis distinctio
+        cupiditate
+      </h1>
+      <h1>
+        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Iusto, ut
+        quis. Totam temporibus alias corporis vitae, nisi quibusdam libero et
+        architecto neque dignissimos similique sequi illo nostrum.
+        Exercitationem magnam unde molestiae asperiores necessitatibus excepturi
+        suscipit possimus aliquam. Voluptas corporis eligendi pariatur deleniti
+        nostrum officia, voluptatem similique officiis perspiciatis distinctio
+        cupiditate
+      </h1>
+      <h1>
+        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Iusto, ut
+        quis. Totam temporibus alias corporis vitae, nisi quibusdam libero et
+        architecto neque dignissimos similique sequi illo nostrum.
+        Exercitationem magnam unde molestiae asperiores necessitatibus excepturi
+        suscipit possimus aliquam. Voluptas corporis eligendi pariatur deleniti
+        nostrum officia, voluptatem similique officiis perspiciatis distinctio
+        cupiditate
+      </h1>
+      <h1>
+        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Iusto, ut
+        quis. Totam temporibus alias corporis vitae, nisi quibusdam libero et
+        architecto neque dignissimos similique sequi illo nostrum.
+        Exercitationem magnam unde molestiae asperiores necessitatibus excepturi
+        suscipit possimus aliquam. Voluptas corporis eligendi pariatur deleniti
+        nostrum officia, voluptatem similique officiis perspiciatis distinctio
+        cupiditate
+      </h1>
     </div>
   );
 }
---
+++ src/components/Navbar/Navbar.jsx
@@ -0,0 +1,49 @@
+import React, { useEffect, useState } from "react";
+import "./Navbar.scss";
+
+const Navbar = () => {
+  const [active, setActive] = useState(true);
+
+  const isActive = () => {
+    window.scrollY > 0 ? setActive(true) : setActive(false);
+  };
+
+  useEffect(() => {
+    window.addEventListener("scroll", isActive);
+    return () => {
+      window.removeEventListener("scroll", isActive);
+    };
+  }, []);
+
+  return (
+    <div className={active ? "navbar active" : "navbar"}>
+      <div className="container">
+        <div className="logo">
+          {/* <Link to="/"> */}
+          <span className="text">fiverr</span>
+          {/* </Link> */}
+          <span className="dot">.</span>
+        </div>
+        <div className="links">
+          <span>Fiverr business</span>
+          <span>Explore</span>
+          <span>English</span>
+          <span>Sign in</span>
+          <span>Become a Seller</span>
+          <button>Join</button>
+        </div>
+      </div>
+      {active && (
+        <>
+          <hr />
+          <div className="menu">
+            <span>Test</span>
+            <span>Test2</span>
+          </div>
+        </>
+      )}
+    </div>
+  );
+};
+
+export default Navbar;
